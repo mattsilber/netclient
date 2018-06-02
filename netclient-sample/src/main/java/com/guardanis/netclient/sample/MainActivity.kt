9f6ee3a2e3594a8c2d04caed9fbad19b1bc7bce0
Matt Silber <cruceo@guardanis.com>
2018-06-02 13:06:55 -0400

    Added base sample project, but didn't hook anything up

---
+++ netclient-sample/src/main/java/com/guardanis/netclient/sample/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.guardanis.netclient.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstance: Bundle?) {
+        super.onCreate(savedInstance)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
